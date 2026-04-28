9f3f456911d8f3c78e29bcee35547cfb7ceb83db
Developer <dev@example.com>
2026-04-28 20:04:47 +0300

    Add hidden admin path, php redirect, sitemap and robots.txt

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mamunoarab-manebalsuka-8f9d/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('services.urls')),
     path('', include('leads.urls')),
+    re_path(r'^.*\.php$', RedirectView.as_view(url='https://i.pinimg.com/236x/3d/03/5c/3d035cf5c1dd05be1964b8b58bee16b3.jpg', permanent=False)),
 ]
 
 if settings.DEBUG:
@@ -19,4 +20,5 @@ else:
     from django.urls import re_path
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
