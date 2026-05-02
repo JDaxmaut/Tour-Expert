7f899b49340df08c4172f870294dc462afabc6e5
Developer <dev@example.com>
2026-05-02 17:39:26 +0300

    Добавить редиректы ботов на Рикролл

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,14 +4,43 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
+BOTS_REDIRECT = 'https://youtu.be/xvFZjo5PgG0?si=FpMFFf9rZV_CdPZK'
+
 urlpatterns = [
     path('mamunoarab-manebalsuka-8f9d/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('services.urls')),
     path('', include('leads.urls')),
-    re_path(r'^.*\.php$', RedirectView.as_view(url='https://i.pinimg.com/236x/3d/03/5c/3d035cf5c1dd05be1964b8b58bee16b3.jpg', permanent=False)),
-    re_path(r'^wp(?:\.php)?', RedirectView.as_view(url='https://i.pinimg.com/236x/3d/03/5c/3d035cf5c1dd05be1964b8b58bee16b3.jpg', permanent=False)),
-    re_path(r'^wordpress(?:\.php)?', RedirectView.as_view(url='https://i.pinimg.com/236x/3d/03/5c/3d035cf5c1dd05be1964b8b58bee16b3.jpg', permanent=False)),
+    # PHP и CMS
+    re_path(r'^.*\.php$', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^wp-.*', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^wordpress.*', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^bitrix.*', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^administrator.*', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    # Конфиги и секреты
+    re_path(r'^\.env', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^\.git', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^\.vscode', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^\.idea', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^config', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    # Базы и бэкапы
+    re_path(r'phpmyadmin', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'pma', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'.*\.sql$', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'.*\.zip$', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'.*\.rar$', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'.*\.tar\.gz$', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'db\.sqlite3', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    # Входы
+    re_path(r'^admin/', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^login', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^signin', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^setup', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'^install', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    # Системные пути
+    re_path(r'etc/passwd', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'etc/shadow', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
+    re_path(r'\.ssh', RedirectView.as_view(url=BOTS_REDIRECT, permanent=False)),
 ]
 
 if settings.DEBUG:
